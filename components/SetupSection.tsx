9eddd446b31d2001245373e5342a00b9c42ee691
gyo82v <gyo82v@gmail.com>
2026-07-25 18:04:19 +0100

    feat: add inputs to the set up component

--- components/SetupSection.tsx
+++ components/SetupSection.tsx
@@ -8,8 +8,35 @@ type Props = {
 export default function SetupSection({setHero}:Props){
     return(
         <section>
-            <form>
-                <p>form here</p>
+            <form className="flex flex-col gap-5 justify-center items-center">
+                <input placeholder="resistenza" type="number" />
+                <input placeholder="combattivita" type="number" />
+                <input placeholder="borsa" type="number" />
+                <input placeholder="oggetti" type="text" />
+                <input placeholder="pasti" type="number" />
+                <input placeholder="oggetti speciali" type="text" />
+                <div>
+                    <input placeholder="bonus name" type="text" />
+                    <input placeholder="bonus value" type="number" />
+                </div>
+                <div>
+                    <select>
+                        <option>Guarigione</option>
+                        <option>Mimetismo</option>
+                        <option>Orientamento</option>
+                        <option>Psicoschermo</option>
+                        <option>Psicolaser</option>
+                        <option>Scherma</option>
+                        <option>Sesto senso</option>
+                        <option></option>
+                        <option></option>
+                        <option></option>
+                    </select>
+                    <button>
+                        add
+                    </button>
+                </div>
+
             </form>
             <button>button here</button>
         </section>
