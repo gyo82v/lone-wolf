398ed4a184a19c218b2bce24ed49df6a96dd9070
gyo82v <gyo82v@gmail.com>
2026-08-04 17:59:09 +0100

    chore: delet unused props

--- components/ui/dividers.tsx
+++ components/ui/dividers.tsx
@@ -1,8 +1,6 @@
-type Props = {
-  className?: string
-}
 
-export function SmallDivider({ className= "" }:Props) {
+
+export function SmallDivider({ className= "" }:{className?: string}) {
   return (
     <div className={`my-4 px-4 ${className}`}>
       <div className="h-px w-full bg-gradient-to-r from-transparent via-stone-500 to-transparent opacity-80" />
