e0d83bbc7960fc18ad60e75ab8436e32dd9067b9
gyo82v <gyo82v@gmail.com>
2026-08-10 16:55:51 +0100

    chore: update button styles

--- styles/button.ts
+++ styles/button.ts
@@ -4,6 +4,4 @@ export const baseButtonStyle =
     hover:bg-stone-600 active:scale-95 
     focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2
     focus-visible:ring-stone-300 focus-visible:ring-offset-stone-300
-    transition-colors ease-in-out duration-300
-    motion-reduce:transition-none motion-reduce:duration-0
-    motion-reduce:transform-none`
+    transition-colors ease-in-out duration-300`
