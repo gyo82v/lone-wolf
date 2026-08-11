c0fc9c3693e68a5e314560f6df6a4a4a0949af88
gyo82v <gyo82v@gmail.com>
2026-08-11 17:57:08 +0100

    chore: update inputs stayle

--- styles/input.ts
+++ styles/input.ts
@@ -5,6 +5,4 @@ export const inputBasestyle =
      focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2
      focus-visible:ring-stone-300 focus-visible:ring-offset-stone-300
      transition-transform ease-in-out duration-300
-     motion-reduce:transition-none motion-reduce:duration-0
-     motion-reduce:transform-none
 `
