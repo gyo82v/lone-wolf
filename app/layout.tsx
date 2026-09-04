e05dd02cd88e2eb7a356d6ae1245deb94355b1b2
gyo82v <gyo82v@gmail.com>
2026-09-04 17:31:26 +0100

    chore: update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Lone wolf",
-  description: "Lone wolf",
+  description: "Lone wolf game simulation",
 };
 
 export default function RootLayout({
